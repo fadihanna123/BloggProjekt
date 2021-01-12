812ed9d5e0d4bbc156d9823105a39b7d5b0df8ed
fadihanna123 <fhanna181@gmail.com>
2021-01-13 00:02:53 +0100

    Typescript Update

--- js/loadAll.ts
+++ js/loadAll.ts
@@ -4,31 +4,32 @@ $(function () {
     .unbind("click")
     .on("click", ".ajax", () => {
       $(".ajaxRequest").empty();
-      loader($(this).attr("data-ajax"));
+      loader($(this).attr("data-ajax")!);
     });
   // Om man trycker på valfri knapp som har input submit och då skickas formulärdata till request mappens filer
   $(document)
     .unbind("submit")
-    .on("submit", "form", (event) => {
+    .on("submit", "form", (event: JQuery.SubmitEvent) => {
       event.preventDefault();
       $.ajax({
         url: "includes/requests/" + $(this).attr("action") + ".php",
         type: "POST",
         data: $(this).serialize(),
       }).done((data) => {
-        $(".ajaxRequest").empty().append(data);
+        $(".ajaxRequest")!.empty().append(data);
       });
     });
 });
+
 // Ladda in innehållet från pages mappens filer
-const loader = (target) => {
-  const req = $("[data-ajax='" + target + "']").attr("data-post");
+const loader = (target: string) => {
+  const req = $("[data-ajax='" + target + "']").attr("data-post")!;
 
   $.ajax({
     url: "includes/loader.php",
     type: "POST",
     data: "page=" + target + "&request=" + req,
   }).done((data) => {
-    $(".ajaxLoader").empty().append(data);
+    $(".ajaxLoader")!.empty().append(data);
   });
 };

b5edcc03ad66abd508a6c64f44e60030fa96cd73
Fadi <fadi@Fadis-MacBook-Pro.local>
2021-04-02 11:24:27 +0200

    Add Sass files

--- includes/classes/posts.class.php
+++ includes/classes/posts.class.php
@@ -1,60 +1,60 @@
 <?php
 
-class Posts
-{protected $_db;
+class Posts {
+    protected $_db;
     // Fixar inställningar för anslutning till databas
-    public function __construct()
-    {
-        ($this->db = (object) new mysqli(DBHOST, DBUSER, DBPASS, DBDATABASE)) or
+
+    public function __construct() {
+        ( $this->db = ( object ) new mysqli( DBHOST, DBUSER, DBPASS, DBDATABASE ) ) or
         die(
-            "Det finns okänt problem - Var vänlig kontakta administratören."
+            'Det finns okänt problem - Var vänlig kontakta administratören.'
         );
     }
 
     // Visa de senaste 5 inlägg
-    public function showPosts(): void
-    {
-        $sql = (string) "SELECT * FROM posts ORDER BY date DESC LIMIT 5";
-        $result = (object)$this->db->query($sql);
-        if ($num = (int)mysqli_num_rows($result) > 0) {
-            while ($row = (array)mysqli_fetch_array($result)) {
+
+    public function showPosts(): void {
+        $sql = ( string ) 'SELECT * FROM posts ORDER BY date DESC LIMIT 5';
+        $result = ( object )$this->db->query( $sql );
+        if ( $num = ( int )mysqli_num_rows( $result ) > 0 ) {
+            while ( $row = ( array )mysqli_fetch_array( $result ) ) {
                 echo "
                     <ul>
                     <li>" .
-                    $row['titel'] .
-                    "<br><i>" .
-                    $row['post'] .
-                    "</i><br> Av: <b>" .
-                    $row['author'] .
-                    "</b>, " .
-                    $row['date'] .
-                    "</li>
+                $row['titel'] .
+                '<br><i>' .
+                $row['post'] .
+                '</i><br> Av: <b>' .
+                $row['author'] .
+                '</b>, ' .
+                $row['date'] .
+                "</li>
                     </ul><br>
                     ";
             }
         } else {
-            echo "Inga inlägg hittills.";
+            echo 'Inga inlägg hittills.';
         }
     }
 
     // Visa egna inlägg med både knapparna Ändra och Ta bort
-    public function Myposts(): void
-    {
-        $id = (int)$this->showId();
-        $sql = (string) "SELECT * FROM posts WHERE usrid=$id";
-        $result = (object)$this->db->query($sql);
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = (array)mysqli_fetch_array($result)) {
+
+    public function Myposts(): void {
+        $id = ( int )$this->showId();
+        $sql = ( string ) "SELECT * FROM posts WHERE usrid=$id";
+        $result = ( object )$this->db->query( $sql );
+        if ( mysqli_num_rows( $result ) > 0 ) {
+            while ( $row = ( array )mysqli_fetch_array( $result ) ) {
                 echo "<div id='posts'>
                         <ul><li> " .
-                    $row['titel'] .
-                    "<br><i>" .
-                    $row['post'] .
-                    "</i><br>Av: " .
-                    $row['author'] .
-                    ", " .
-                    $row['date'] .
-                    "
+                $row['titel'] .
+                '<br><i>' .
+                $row['post'] .
+                '</i><br>Av: ' .
+                $row['author'] .
+                ', ' .
+                $row['date'] .
+                "
                         <form action='edit'>
                             <input type='hidden' name='postId' value='{$row['id']}' />
                             <input type='submit' class='change' value='Ändra'> &nbsp;
@@ -66,38 +66,39 @@ class Posts
                         <br></li></div></ul>";
             }
         } else {
-            echo "Du har inga inlägg.";
+            echo 'Du har inga inlägg.';
         }
     }
 
     // Hämta alla data från databas enligt id och där id är argument
-    public function getInfo(int $id): array
-    {
-        $sql = (string) "SELECT * FROM posts WHERE id=$id";
-        $result = (object)$this->db->query($sql);
-        $row = (array)mysqli_fetch_array($result);
+
+    public function getInfo( int $id ): array {
+        $sql = ( string ) "SELECT * FROM posts WHERE id=$id";
+        $result = ( object )$this->db->query( $sql );
+        $row = ( array )mysqli_fetch_array( $result );
         return $row;
     }
 
     // Gör ta bort inlägg processen
-    public function Delete(int $id): void
-    {
-        $sql = (string) "DELETE FROM posts WHERE id=$id";
-        if ($this->db->query($sql)) {
-            echo "Klart!";
+
+    public function Delete( int $id ): void {
+        $sql = ( string ) "DELETE FROM posts WHERE id=$id";
+        if ( $this->db->query( $sql ) ) {
+            echo 'Klart!';
         } else {
-            echo "Fel...";
+            echo 'Fel...';
         }
     }
 
     // Gör ändra inlägg processen
+
     public function Edit(
         string $title,
         string $post,
         int $id
     ): void {
-        $sql = (string) "UPDATE posts SET titel='{$title}', post='{$post}' WHERE id='{$id}'";
-        if ($this->db->query($sql)) {
+        $sql = ( string ) "UPDATE posts SET titel='{$title}', post='{$post}' WHERE id='{$id}'";
+        if ( $this->db->query( $sql ) ) {
             print 'Klart.';
         } else {
             print '<div id="error">Något gick fel.</div>';
@@ -105,140 +106,143 @@ class Posts
     }
 
     // Hämta fullständigt namn från databas enligt id som kommer från funktionen showid
-    public function info(): string
-    {
-        $id = (int)$this->showId();
-        $sql = (string) "SELECT * FROM users WHERE id=$id";
-        $result = (object)$this->db->query($sql);
-        $row = (array)mysqli_fetch_array($result);
+
+    public function info(): string {
+        $id = ( int )$this->showId();
+        $sql = ( string ) "SELECT * FROM users WHERE id=$id";
+        $result = ( object )$this->db->query( $sql );
+        $row = ( array )mysqli_fetch_array( $result );
         return $row['fullnamn'];
     }
 
     // Hämta användare id från databas enligt id som kommer från session
-    public function showId(): int
-    {
-        $email = (string) isset($_SESSION['email']) ? $_SESSION['email'] : '';
-        $sql = (string) "SELECT id FROM users WHERE email='$email';";
-        $result = (object)$this->db->query($sql);
-        $row = (array)mysqli_fetch_array($result);
-        return (int)$row['id'];
+
+    public function showId(): int {
+        $email = ( string ) isset( $_SESSION['email'] ) ? $_SESSION['email'] : '';
+        $sql = ( string ) "SELECT id FROM users WHERE email='$email';";
+        $result = ( object )$this->db->query( $sql );
+        $row = ( array )mysqli_fetch_array( $result );
+        return ( int )$row['id'];
     }
 
     // Leta efter sökordet i databasen
-    public function search(string $ord): void
-    {
-        $sql = (string) "SELECT * FROM posts WHERE post LIKE '%$ord%' OR titel LIKE '%$ord%' OR author LIKE '%$ord%';";
-        $result = (object)$this->db->query($sql);
-        $num = (int)mysqli_num_rows($result);
-        if ($num > 0) {
-            while ($row = (array)mysqli_fetch_array($result)) {
-                echo "<br><ul><li>" .
-                    $row['titel'] .
-                    "<br><u>" .
-                    $row['post'] .
-                    "</u><br>Av: " .
-                    $row['author'] .
-                    ", " .
-                    $row['date'] .
-                    "</li></ul><br>";
+
+    public function search( string $ord ): void {
+        $sql = ( string ) "SELECT * FROM posts WHERE post LIKE '%$ord%' OR titel LIKE '%$ord%' OR author LIKE '%$ord%';";
+        $result = ( object )$this->db->query( $sql );
+        $num = ( int )mysqli_num_rows( $result );
+        if ( $num > 0 ) {
+            while ( $row = ( array )mysqli_fetch_array( $result ) ) {
+                echo '<br><ul><li>' .
+                $row['titel'] .
+                '<br><u>' .
+                $row['post'] .
+                '</u><br>Av: ' .
+                $row['author'] .
+                ', ' .
+                $row['date'] .
+                '</li></ul><br>';
             }
         } else {
-            echo "<br>Inga resultat hittades!<br>";
+            echo '<br>Inga resultat hittades!<br>';
         }
     }
 
     // Visa användare inlägg enligt id som argument
-    public function showPost($id): void
-    {
-        $sql = (string) "SELECT * FROM posts WHERE usrid=$id;";
-        $result = (object)$this->db->query($sql);
-        if (!isset($_SESSION['email'])) {
+
+    public function showPost( $id ): void {
+        $sql = ( string ) "SELECT * FROM posts WHERE usrid=$id;";
+        $result = ( object )$this->db->query( $sql );
+        if ( !isset( $_SESSION['email'] ) ) {
             echo "<script>alert('Du måste vara inloggad för att se denna sida');</script>";
             echo '<meta http-equiv="refresh" content="1; url=index.php">';
         }
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = (array)mysqli_fetch_array($result)) {
+        if ( mysqli_num_rows( $result ) > 0 ) {
+            while ( $row = ( array )mysqli_fetch_array( $result ) ) {
                 echo "<div id='posts'>
                         <ul><li> " .
-                    $row['titel'] .
-                    "<br><i>" .
-                    $row['post'] .
-                    "</i><br>Av: " .
-                    $row['author'] .
-                    ", " .
-                    $row['date'] .
-                    "
+                $row['titel'] .
+                '<br><i>' .
+                $row['post'] .
+                '</i><br>Av: ' .
+                $row['author'] .
+                ', ' .
+                $row['date'] .
+                "
                         <br></li></div></ul>";
             }
         } else {
-            echo "Inga inlägg hittills.";
+            echo 'Inga inlägg hittills.';
         }
     }
 
     // Visa alla de registrerade användare
-    public function showUsers(): void
-    {
-        $sql = (string) "SELECT * FROM users";
-        $result = (object)$this->db->query($sql);
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = (array)mysqli_fetch_array($result)) {
+
+    public function showUsers(): void {
+        $sql = ( string ) 'SELECT * FROM users';
+        $result = ( object )$this->db->query( $sql );
+        if ( mysqli_num_rows( $result ) > 0 ) {
+            while ( $row = ( array )mysqli_fetch_array( $result ) ) {
                 echo "
                         <ul class='list'>
                             <li><a class='ajax' id='doit' data-ajax='showposts' data-post='{$row['id']}'>$row[username]</a></li>
                         </ul>";
             }
         } else {
-            echo "Inga användare regisrerade hittills!";
+            echo 'Inga användare regisrerade hittills!';
         }
     }
 
     // Gör registreringsprocessen genom att spara användaredata i databasen
+
     public function doregister(
         string $fullname,
         string $username,
         string $password,
         string $email
     ): void {
-        $now = (string)date('Y-m-d H:i:s', time());
+        $now = ( string )date( 'Y-m-d H:i:s', time() );
 
-        $sql = (string) "INSERT INTO users (fullnamn, username, password, email, date) VALUES('$fullname', '$username', '$password', '$email', '$now')";
-        if ($this->db->query($sql)) {
+        $sql = ( string ) "INSERT INTO users (fullnamn, username, password, email, date) VALUES('$fullname', '$username', '$password', '$email', '$now')";
+        if ( $this->db->query( $sql ) ) {
             echo 'Tack för registrering<meta http-equiv="refresh" content="2; url=index.php">';
         } else {
-            echo mysqli_error($this->db);
+            echo mysqli_error( $this->db );
             echo "<div class='error'>Ett okänt fel dök upp. Var vänlig kontakta administratören.</div>";
         }
     }
 
     // Gör lägga till processen genom att spara dessa data i databasen
+
     public function Add(
         string $author,
         string $title,
         string $content,
         int $usrid
     ): void {
-        $now = (string)date('Y-m-d H:i:s', time());
+        $now = ( string )date( 'Y-m-d H:i:s', time() );
 
-        $sql = (string) "INSERT INTO posts (titel,post,author,date,usrid) VALUES ('$title', '$content', '$author', '$now','$usrid');";
-        if ($this->db->query($sql)) {
+        $sql = ( string ) "INSERT INTO posts (titel,post,author,date,usrid) VALUES ('$title', '$content', '$author', '$now','$usrid');";
+        if ( $this->db->query( $sql ) ) {
             echo 'Dina data har lagts till. <span href="admin" class="link ajax" data-ajax="admin">Gå tillbaka</span>';
         } else {
-            echo mysqli_error($this->db);
+            echo mysqli_error( $this->db );
             echo "<div class='error'>Ett okänt fel dök upp. Var vänlig kontakta administratören.</div>";
         }
     }
 
     // Kontrollera om användarnamet och lösenordet finns i databas eller inte
+
     public function check(
         string $username,
         string $password
     ): void {
-        $sql = (string) "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $sql = ( string ) "SELECT * FROM users WHERE username='$username' AND password='$password'";
 
-        $result = (object)$this->db->query($sql);
-        $num = (int)mysqli_num_rows($result);
-        $row = (array)mysqli_fetch_array($result);
-        if ($num > 0) {
+        $result = ( object )$this->db->query( $sql );
+        $num = ( int )mysqli_num_rows( $result );
+        $row = ( array )mysqli_fetch_array( $result );
+        if ( $num > 0 ) {
             echo '<div class="main">Du blev inloggad</div><meta http-equiv="refresh" content="1; url=index.php">';
             $_SESSION['email'] = $row['email'];
         } else {
--- index.php
+++ index.php
@@ -1,36 +1,39 @@
 <?php
 
-require "includes/config.php";
-$posts = (object) new Posts();
+require 'includes/config.php';
+$posts = ( object ) new Posts();
 ?>
 <!-- Utvecklare: Fadi Hanna -->
 <!DOCTYPE html>
-<html lang="sv">
-    <head>
-        <title>Blogging</title>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
-        <link rel="stylesheet" type="text/css" href="css/main.css?t=<?php echo time() ?>" />
-        <link rel="stylesheet" type="text/css" href="css/mobile.css" />
-    </head>
-    <body>
-        <!-- Lägg till Container inställningar -->
-        <div id="container">
-            <!-- Lägg till Logotypens delen -->
-            <div class="logobox"></div>
-             <!-- Lägg ramen-->
-            <div class="maintable">
+<html lang='sv'>
 
-                 <!-- Lägg till vänsterdelen -->
-                <div class="maintable-left">
-                     <!-- Lägg till navigationslänkar -->
-                    <div class="menu-left">
-                        <span onclick="window.location = 'index.php'">Startsida</span>
-<?php
+<head>
+    <title>Blogging</title>
+    <meta charset='utf-8' />
+    <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'
+        integrity='sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=='
+        crossorigin='anonymous' />
+    <link rel='stylesheet' type='text/css' href='css/_main.min.css?t=<?php echo time() ?>' />
+</head>
 
-if (isset($_SESSION['email'])) {
+<body>
+    <!-- Lägg till Container inställningar -->
+    <div id='container'>
+        <!-- Lägg till Logotypens delen -->
+        <div class='logobox'></div>
+        <!-- Lägg ramen-->
+        <div class='maintable'>
+
+            <!-- Lägg till vänsterdelen -->
+            <div class='maintable-left'>
+                <!-- Lägg till navigationslänkar -->
+                <div class='menu-left'>
+                    <span onclick="window.location = 'index.php'">Startsida</span>
+                    <?php
+
+if ( isset( $_SESSION['email'] ) ) {
     echo "<span class='ajax' data-ajax='create'>Skapa inlägg</span>";
     echo "<span class='ajax' data-ajax='admin'>Administrera inlägg</span>";
     echo "<span class='ajax' data-ajax='logout'>Logga ut</span>";
@@ -39,16 +42,16 @@ if (isset($_SESSION['email'])) {
 }
 
 ?>
-                    </div>
                 </div>
-                 <!-- Lägg till centerdelen -->
-                <div class="maintable-center">
-                     <!-- Här kommer innehållet av pages mappens filer -->
-                    <div class="ajaxRequest"></div>
-                     <!-- Lägg till de senaste 5 inlägg och de registrerade användare -->
-                    <div class="ajaxLoader">
+            </div>
+            <!-- Lägg till centerdelen -->
+            <div class='maintable-center'>
+                <!-- Här kommer innehållet av pages mappens filer -->
+                <div class='ajaxRequest'></div>
+                <!-- Lägg till de senaste 5 inlägg och de registrerade användare -->
+                <div class='ajaxLoader'>
 
-<?php
+                    <?php
 
 echo '<div>De senaste 5 inlägg</div>';
 // Visa de senaste 5 inlägg
@@ -58,19 +61,19 @@ echo '<div>De registrerade användare</div>';
 $posts->showUsers();
 
 ?>
-                    </div>
                 </div>
-                 <!-- Lägg till högerdelen -->
-                <div class="maintable-right">
-                     <!-- Sökdelen -->
-                    <div class='searchbox2'>
-                        <form action="search" method="post">
-                            Sök här efter inlägg<br>
-                            <input type="text" name="word" id="word" placeholder="Sök här" required />
-                            <input type="submit" name="searchbtn" value="Sök" class='search' />
-                        </form>
-                    </div>
-                    <?php if (!isset($_SESSION['email'])) {
+            </div>
+            <!-- Lägg till högerdelen -->
+            <div class='maintable-right'>
+                <!-- Sökdelen -->
+                <div class='searchbox2'>
+                    <form action='search' method='post'>
+                        Sök här efter inlägg<br>
+                        <input type='text' name='word' id='word' placeholder='Sök här' required />
+                        <input type='submit' name='searchbtn' value='Sök' class='search' />
+                    </form>
+                </div>
+                <?php if ( !isset( $_SESSION['email'] ) ) {
     // Visa logga in delen
     echo '<div class="loginbox2">
                             Logga in<br>
@@ -82,15 +85,17 @@ $posts->showUsers();
                             <input type="submit" name="loginbtn" class="login" value="Logga in">
                         </form>
                         </div>';
-} ?>
-                </div>
-
+}
+?>
             </div>
-             <!-- Visa sidfoten -->
-            <div class="footer">Skapad av Fadi</div>
 
         </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="js/loadAll.min.js?t=<?php echo time() ?>"></script>
-    </body>
+        <!-- Visa sidfoten -->
+        <div class='footer'>Skapad av Fadi</div>
+
+    </div>
+    <script src='https://code.jquery.com/jquery-3.5.1.min.js'></script>
+    <script src='js/loadAll.min.js?t=<?php echo time() ?>'></script>
+</body>
+
 </html>
